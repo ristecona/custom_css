0da6e04cca6e77fc507409a967a640ae3c4a5de9
Cona <ristecona@gmail.com>
2017-01-24 11:13:38 +0100

    Custom CSS for Odoo Website

---
+++ custom_css/__manifest__.py
@@ -0,0 +1,10 @@
+{
+     'name': "Custom CSS",
+     'description': "Add your custom CSS in the static/src/css",
+     'category': 'Generic Modules/Others',
+     'depends': ['web'],
+     'data': ['views/view.xml',],
+     'css': ['static/src/css/my_css.css'],
+     'price': 29.99,
+     'currency': 'EUR',
+ }
